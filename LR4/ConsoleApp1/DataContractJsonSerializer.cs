d8f363c91fee86ac205f04093f094d91a10320cb
Deker39 <bigtigerlesha@gmail.com>
2018-12-06 21:15:27 +0200

    LR4

---
+++ LR4/ConsoleApp1/DataContractJsonSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class DataContractJsonSerializer
+    {
+        private Type type;
+
+        public DataContractJsonSerializer(Type type)
+        {
+            this.type = type;
+        }
+
+        internal void WriteObject(FileStream fs, Person[] people)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Person[] ReadObject(FileStream fs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
